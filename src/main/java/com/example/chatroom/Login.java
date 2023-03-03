ed3175837343fadd5b9923f928e34bddfe6f80bb
122040011 <122040011@link.cuhk.edu.cn>
2023-03-03 17:13:16 +0800

    Initial version

--- src/main/java/com/example/chatroom/Login.java
+++ src/main/java/com/example/chatroom/Login.java
@@ -11,7 +11,7 @@ public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 608, 382);
         stage.setTitle("MeChat");
         stage.setScene(scene);
         stage.show();
